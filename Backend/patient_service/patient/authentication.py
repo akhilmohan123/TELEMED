e2a05fdc36d7fb400c3c7c6c681e56bcd7c96d63
akhilmohan123 <akhilmohan8547@gmail.com>
2025-10-19 16:33:58 +0530

    Initailized a patient service app

---
+++ Backend/patient_service/patient/authentication.py
@@ -0,0 +1,48 @@
+import jwt
+from django.conf import settings
+from rest_framework import authentication
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth import get_user_model
+with open("C:/Users/user/Desktop/akhil/public.pem", "r") as f:
+    public_key = f.read()
+User = get_user_model()
+class AuthServiceUser:
+    """
+    Represents a user from auth service for DRF
+    """
+    def __init__(self, id, email):
+        self.id = id
+        self.email = email
+
+    @property
+    def is_authenticated(self):
+        return True
+class JWTAuthentication(authentication.BaseAuthentication):
+    
+    def authenticate(self, request):
+        # auth_header = request.headers.get('Authorization')
+        
+        # if not auth_header:
+        #     return None
+        print("JWTAuthentication called")
+
+        try:
+            token = request.COOKIES.get('access_token')
+            print("Extracted token:", token)
+            if not token:
+                return None
+            payload = jwt.decode(token, public_key, algorithms=["RS256"])
+            print(payload)
+            id=payload.get("id")
+            email=payload.get("email")
+            user = AuthServiceUser(id, email)
+            return (user, token)
+        except jwt.ExpiredSignatureError:
+            print(" Token expired")
+            raise AuthenticationFailed("Token expired")
+        except jwt.DecodeError:
+            print(" Invalid token")
+            raise AuthenticationFailed("Invalid token")
+        except Exception as e:
+            print("Error decoding token:", e)
+            raise AuthenticationFailed("Invalid token")
---
+++ Backend/patient_service/patient/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+import random
+# Create your models here.
+
+
+class PatientProfile(models.Model):
+    def create_new_ref_number():
+        return str(random.randint(10000,90000))
+    user=models.UUIDField(unique=True)
+    phone_number=models.CharField(max_length=15)
+    date_of_birth=models.DateField(null=True,blank=True)
+    address=models.TextField(blank=True)
+    medical_history=models.TextField()
+    additional_information=models.TextField()
+   
+    def __str__(self):
+        return self.user.email
