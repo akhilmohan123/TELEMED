8d9436c5d4e87d508742fef65cc88ffdf77358ca
akhilmohan123 <akhilmohan8547@gmail.com>
2026-03-17 22:29:18 +0530

    made changes in frontend for frontend prod

--- frontend/src/pages/axios/axios.js
+++ frontend/src/pages/axios/axios.js
@@ -1,34 +1,40 @@
 import axios from 'axios';
 import { attachAuthInterceptors } from './interceptors';
+const AUTH_BASEURL = import.meta.env.AUTH_BASEURL || "http://localhost";
+const DOCTOR_BASEURL = import.meta.env.DOCTOR_BASEURL || "http://localhost";
+const PATIENT_BASEURL = import.meta.env.PATIENT_BASEURL || "http://localhost";
+const APPOINT_BASEURL = import.meta.env.APPOINT_BASEURL || "http://localhost";
+const MEDIA_BASEURL = import.meta.env.MEDIA_BASEURL || "http://localhost";
+const PAYMENT_BASEURL = import.meta.env.PAYMENT_BASEURL || "http://localhost";
 // Auth API
 export const auth_api = axios.create({
-    baseURL: "/users/api"
+    baseURL: `${AUTH_BASEURL}/users/api`
 });
 
 // Doctor API
 export const doctor_api = axios.create({
-    baseURL: "/doctor/api"
+    baseURL: `${DOCTOR_BASEURL}/doctor/api`
 });
 
 // Patient API
 export const patient_api = axios.create({
-    baseURL: "/patient/api"
+    baseURL: `${PATIENT_BASEURL}/patient/api`
 });
 
 // Appointment API
 export const appointment_api = axios.create({
-    baseURL: "/appointment/api"
+    baseURL: `${APPOINT_BASEURL}/appointment/api`
 });
 
 // Media API (keep full URL only if media is not behind Nginx)
 export const media_api = axios.create({
-    baseURL: "/media/api"  // if routed through nginx
+    baseURL: `${MEDIA_BASEURL}/media/api`  // if routed through nginx
 });
 
 
 // Payment API (keep full URL only if media is not behind Nginx)
 export const payment_api = axios.create({
-    baseURL: "/payment/api"  // if routed through nginx
+    baseURL: `${PAYMENT_BASEURL}/payment/api`  // if routed through nginx
 });
 [
   doctor_api,
