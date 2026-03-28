690dfd61303d12c5d0d6d6be0518dbae6d14e299
akhilmohan123 <akhilmohan8547@gmail.com>
2026-03-28 13:23:29 +0530

    change in videocall view regarding ping

--- Backend/media_service/videocall/views.py
+++ Backend/media_service/videocall/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-
+from rest_framework.views import APIView
 from rest_framework import status
 # Create your views here.
 class Video_Call_View:
